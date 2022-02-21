9f6646dc88728c22fba2375af81fcd510fbfc838
umtyzc <umut.fiwc@gmail.com>
2022-02-22 00:53:39 +0300

    Delete zhvl_fi_muhtasar directory

--- zhvl_fi_muhtasar/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
-    return AppComponent.extend("com.hvl.fi.zhvlfimuhtasar.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
